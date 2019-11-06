8dd6929df8346f0c17fbfe0865f0e549de1eb881
Harper <harperliulove@gmail.com>
2019-11-06 13:24:48 +1100

    changing another way of getting location

--- main.js
+++ main.js
@@ -92,6 +92,15 @@ function locationError(error) {
 
 navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
 
+var movie = "Mr. Nobody";
+var queryURL = "https://api.opencagedata.com/geocode/v1/json?q="+latitude+"+"+longitude+"&key=f1e329be05284ac6aa0cee3165fa5fff&pretty=1";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function(response) {
+	console.log(response);
+	});
 
 $(document).ready(function(){
 	setInterval(function(){
@@ -124,52 +133,4 @@ $(document).ready(function(){
 			}
 		});
 	},100);
-
-	// Instantiate a map and platform object:
-	var platform = new H.service.Platform({
-		'apikey': 'UKEHAbFIp4R1pJ3NTwJL'
-	});
-	// Retrieve the target element for the map:
-	var targetElement = document.getElementById('mapContainer');
-	// Get default map types from the platform object:
-	var defaultLayers = platform.createDefaultLayers();
-
-	// Instantiate the map:
-	var map = new H.Map(
-		document.getElementById('mapContainer'),
-		defaultLayers.vector.normal.map,
-		{
-		zoom: 10,
-		center: { lat: latitude, lng: longitude }
-		});
-	console.log(map);
-	// Create the parameters for the reverse geocoding request:
-	var reverseGeocodingParameters = {
-		prox: latitude,longitude,
-		mode: 'retrieveAddresses',
-		maxresults: 1
-		};
-
-	// Define a callback function to process the response:
-	function onSuccess(result) {
-		var location = result.Response.View[0].Result[0];
-
-		// Create an InfoBubble at the returned location with
-		// the address as its contents:
-		ui.addBubble(new H.ui.InfoBubble({
-		lat: location.Location.DisplayPosition.Latitude,
-		lng: location.Location.DisplayPosition.Longitude
-		}, { content: location.Location.Address.Label }));
-	};
-
-	// Get an instance of the geocoding service:
-	var geocoder = platform.getGeocodingService();
-
-	// Call the geocode method with the geocoding parameters,
-	// the callback and an error callback function (called if a
-	// communication error occurs):
-	geocoder.reverseGeocode(
-		reverseGeocodingParameters,
-		onSuccess,
-		function(e) { alert(e); });
 });
